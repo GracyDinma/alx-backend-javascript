f5d2796bc8e29ce0158d6fe3993c0df4623bc35a
GracyDinma <okohagchidinma@gmail.com>
2024-11-26 10:50:08 +0000

    using prcesses updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ console.log("Welcome to Holberton School, what is your name?");
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.stdin.pause();
+  process.stdin.end();
 });
 
 // Display the closing message when the process ends
