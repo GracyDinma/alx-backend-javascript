1e3e4d9e35b4b01c35cddca2a222aa8ece577ffb
GracyDinma <okohagchidinma@gmail.com>
2024-12-30 12:00:02 +0000

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,14 @@
-const getFullREsponseFromAPI = (success) => {
+function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: 'Success' });
+      resolve({
+	status: 200,
+	body: 'Success'
+      });
     } else {
       reject(new Error('THe Fake API is not working currently'));
     }
   });
-};
+}
 
-export default getFullResponseFromAPI;
+module.exports = getFullResponseFromAPI;
