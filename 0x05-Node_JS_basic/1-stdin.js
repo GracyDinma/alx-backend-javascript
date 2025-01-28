3e6849729b6bdbe78651d6db81718288af43b769
GracyDinma <okohagchidinma@gmail.com>
2025-01-28 09:54:34 +0000

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
-process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}\n`);
   process.stdin.end();
 });
 
